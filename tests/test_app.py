061379e341a536cc3b4ce4571b4bd9b257e2275c
otavmacedo <otavmacedo04@gmail.com>
2024-12-23 18:42:02 -0300

    feat: adding tests for create and read users - adding first schemas - provisional

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,4 +5,18 @@ def test_hello_world(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'detail': 'Hello World'}
+    assert response.json() == {'message': 'Hello World'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users', json={'username': 'otavio', 'email': 'otavmacedo04@gmail.com', 'password': '1234'}
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {'username': 'otavio', 'email': 'otavmacedo04@gmail.com'}
+
+
+def test_read_users(client):
+    response = client.get('/users')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [{'username': 'otavio', 'email': 'otavmacedo04@gmail.com'}]}
