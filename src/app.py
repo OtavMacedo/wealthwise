188a80c467a0b16423445daef6a2332d9def5795
otavmacedo <otavmacedo04@gmail.com>
2024-12-23 18:40:58 -0300

    feat: adding first routes

--- src/app.py
+++ src/app.py
@@ -1,8 +1,25 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from src.schemas import Message, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
+data_base = []
 
-@app.get('/')
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def hello_world():
-    return {'detail': 'Hello World'}
+    return {'message': 'Hello World'}
+
+
+@app.post('/users', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    data_base.append(user)
+    return user
+
+
+@app.get('/users', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': data_base}
